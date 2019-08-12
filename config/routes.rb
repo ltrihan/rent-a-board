407fa6dadfe899b20a2d8c726d078c1048a237df
Loic Trihan <lolotrihan@hotmail.fr>
2019-08-12 18:33:12 +0200

    add routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,10 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  resources :boards do
+    resources :bookings, only: [:new, :create, :destroy]
+  end
+
+  resources :users, only: [:show]
 end
