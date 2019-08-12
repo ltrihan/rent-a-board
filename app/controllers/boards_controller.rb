7c580eaf96e44359ae45197c0b6d73b1aba42806
Loic Trihan <lolotrihan@hotmail.fr>
2019-08-12 20:22:30 +0200

    work on boards index & show + fullname method in user model + boards controller done

--- app/controllers/boards_controller.rb
+++ app/controllers/boards_controller.rb
@@ -4,4 +4,48 @@ class BoardsController < ApplicationController
   def index
     @boards = Board.all
   end
+
+  def show
+    @board = Board.find(params[:id])
+  end
+
+  def new
+    @board = Board.new
+  end
+
+  def create
+    @board = Board.new(board_params)
+    @user = current_user
+    @board.user = @user
+    if @board.save
+      redirect_to user_path(@user)
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @board = Board.find(params[:id])
+  end
+
+  def update
+    @board = Board.find(params[:id])
+    if @board.save
+      redirect_to user_path(@user)
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @board = Board.find(params[:id])
+    @board.destroy
+    redirect_to user_path(@user)
+  end
+
+  private
+
+  def board_params
+    params.require(:board).permit(:price, :category, :photo)
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,8 @@ class User < ApplicationRecord
 
   has_many :boards
   has_many :bookings
+
+  def full_name
+    "#{first_name} #{last_name}"
+  end
 end
